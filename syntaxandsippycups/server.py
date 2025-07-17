45383a48193477dc4521fc017f7533981bd5f8c7
Raiven <rjohns42@hawk.iit.edu>
2025-07-17 12:00:03 -0400

    include jsonify

--- syntaxandsippycups/server.py
+++ syntaxandsippycups/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, jsonify
 from flask_mail import Mail
 from markupsafe import Markup
 
