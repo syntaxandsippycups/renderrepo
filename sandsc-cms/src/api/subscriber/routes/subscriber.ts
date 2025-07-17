db2f6fdd49f7c07a2d533ff873979661cd1edfff
Raiven <rjohns42@hawk.iit.edu>
2025-07-17 15:23:20 -0400

    need a nap

--- sandsc-cms/src/api/subscriber/routes/subscriber.ts
+++ sandsc-cms/src/api/subscriber/routes/subscriber.ts
@@ -5,7 +5,6 @@ export default {
       path: '/subscribers',
       handler: 'subscriber.create',
       config: {
-        auth: false, // or true if you want it protected
         policies: [],
         middlewares: [],
       },
