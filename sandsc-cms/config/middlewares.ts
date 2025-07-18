5b41dbf3e20d648bc9c017c1eba5845a7dae202b
Raiven <rjohns42@hawk.iit.edu>
2025-07-17 23:39:07 -0400

    displa

--- sandsc-cms/config/middlewares.ts
+++ sandsc-cms/config/middlewares.ts
@@ -1,7 +1,23 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://res.cloudinary.com', // 👈 Allow Cloudinary images
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
